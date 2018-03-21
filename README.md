2773404055921d49c1664ac1737c221ee0c9e99e
David Venin <david@hostnfly.com>
2018-03-21 16:25:51 +0100

    Starter for React-pwa-offline-cssnext

---
+++ README.md
@@ -0,0 +1,6 @@
+# React PWA with offline features & cssnext
+
+## CSS Env
+- Use of [cssnext](http://cssnext.io/usage/)
+- Use of [CSS Modules](https://github.com/css-modules/css-modules)
+- Use of envConfig file based on `process.env.DEPLOY`
---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.css';
+
+const App = () => (
+  <section className={styles.App}>
+    <header className={styles.AppHeader}>
+      <h1>React PWA with Offline features & cssnext</h1>
+    </header>
+    <p className={styles.AppIntro}>App is ready</p>
+  </section>
+);
+
+export default App;
